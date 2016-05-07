53187ab7141a6d0d97b411ff12f90aecd7a46d9a
Veera <veera.k.sundar@gmail.com>
2016-05-07 16:38:29 -0700

    Add animation to the social icons.

--- sandbox/index.js
+++ sandbox/index.js
@@ -1,1 +1,30 @@
 require('./main.scss');
+
+(function () {
+    "use strict";
+
+    document.onreadystatechange = function () {
+        if (document.readyState == "complete") {
+            init()
+        }
+    }
+
+    function init() {
+        animateSocialIcons();
+    }
+
+    function animateSocialIcons() {
+        var icons = document.querySelectorAll('.social-links li a i'),
+            numOfIcons = icons.length,
+            i;
+        for (i = 0; i < numOfIcons; i++) {
+            (function (i) {
+                setTimeout(function () {
+                    icons[i].classList.add('animated', 'bounceIn');
+                }, i * 100);
+            }(i));
+        }
+    }
+
+
+}());
